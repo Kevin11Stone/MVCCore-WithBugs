5e297ca99716595e76c041f7a5a91958f87057c0
Kevin Stone <ruca6616@gmail.com>
2019-08-14 16:56:59 -0700

    Add project files.

---
+++ MVCCore-WithBugs/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCCore_WithBugs.Models;
+
+namespace MVCCore_WithBugs.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductContext _context;
+
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<ActionResult> Index()
+        {
+            List<Product> allProducts = await ProductDb.GetAllProducts(_context);
+            return View(allProducts);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await ProductDb.AddProduct(_context, product);
+                return RedirectToAction("Index");
+            }
+
+            //return model back to view with errors
+            return View();
+        }
+    }
+}
---
+++ MVCCore-WithBugs/Models/ProductDb.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCore_WithBugs.Models
+{
+    public static class ProductDb
+    {
+        public static async Task<List<Product>> GetAllProducts(ProductContext _context)
+        {
+            List<Product> listOfProducts = await _context.Product.
+                                                OrderBy(p => p.ProductName)
+                                                .ToListAsync();
+            return listOfProducts;
+        }
+
+        public static async Task<Product> AddProduct(ProductContext context, Product prod)
+        {
+            await context.Product.AddAsync(prod);
+            await context.SaveChangesAsync();
+            return prod;
+        }
+    }
+}
